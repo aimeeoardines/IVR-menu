cf26b8a8255a97c3a2cbfc92b6f7e42ffe9d32b4
Aimee <aimee.o.ardines@gmail.com>
2016-07-24 01:17:55 +0800

    add main menu and sub menu

--- assets/js/ivr.js
+++ assets/js/ivr.js
@@ -1,9 +1,24 @@
-var virApp = angular.module('virApp', []);
+var ivrApp = angular.module('ivrApp', []);
 
-virApp.controller('VirCtrl', function ($scope) {
-	$scope.btnNumbers = [
-		{'name' : 'one'},
-		{'name': 'two'},
-		{'name': 'three'}
-	];
+ivrApp.controller('IvrCtrl', function ($scope) {
+	$scope.menuBtns = [[],[],[]];
+	$scope.menuName = 'IVR MENU';
+
+	$scope.addMenu = function(){
+		$scope.menuBtns.push([]);
+
+	};
+
+	$scope.removeMenu = function(){
+		$scope.menuBtns.pop([]);
+	};
+
+	$scope.showSubMenu = function(index, test){
+		$scope.menuName = index + 1;
+		// $scope.parentBtnName = test;
+		$scope.menuBtns = $scope.menuBtns[index];
+	};
+
+
 });
+
